9f1958ae02d186fb90f8711639ff9b59598ac44b
gumob <gumob.dev@gmail.com>
2024-08-25 16:58:56 +0900

    build: Fixed a issue that prevented building projects with Xcode 15.4 in arm64 environment.

---
+++ Source/TLDExtractSwift.h
@@ -0,0 +1,18 @@
+//
+//  TLDExtractSwift.h
+//  TLDExtractSwift
+//
+//  Created by Kojiro Futamura on 2024/08/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TLDExtractSwift.
+FOUNDATION_EXPORT double TLDExtractSwiftVersionNumber;
+
+//! Project version string for TLDExtractSwift.
+FOUNDATION_EXPORT const unsigned char TLDExtractSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TLDExtractSwift/PublicHeader.h>
+
+
